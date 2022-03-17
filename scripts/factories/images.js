4211d6c8bb175532906df00dd2fe17719e769be5
Poph-Digital <propjhh>
2022-03-17 12:59:53 +0100

    edit gallery section

--- scripts/factories/images.js
+++ scripts/factories/images.js
@@ -1,18 +1,16 @@
 function imagePhotographerFactory(data) {
-    const { title, image, likes, name } = data;
+    const { title, image, likes } = data;
 
-    const pictureMedia = `assets/photographers/${name}/${image}`;
+    const pictureMedia = `assets/photographers/${image}`;
 
     function getImagePhotographers() {
         const sectionImage = document.createElement('section');
         sectionImage.classList.add('medias-photographer');
         sectionImage.innerHTML =
-            `<div>
-                <img src="${pictureMedia}" alt="Images du photographe">
-                <div>
-                    <h2 class="photographer_infos_name">${title}</h2>
-                    <span>${likes} <span class="red-likes">❤</span></span>
-                </div>
+            `<img src="${pictureMedia}" alt="Images du photographe">
+            <div class="media-bottom">
+                <h2 class="photographer_infos_name">${title}</h2>
+                <span>${likes} <span class="red-likes">❤</span></span>
             </div>`;
         return (sectionImage);
     }
